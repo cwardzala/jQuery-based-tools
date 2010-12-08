96638c1495d4d0b2958efc5ca45556d7851915be
Cameron Wardzala <cwardzala@gmail.com>
2010-12-08 08:50:11 -0500

    Updated Accessible Tabs to the latest
    Signed-off-by: Cameron Wardzala <cwardzala@gmail.com>

--- AccessibleTabs/tabs.plugin.js
+++ AccessibleTabs/tabs.plugin.js
@@ -1,30 +1,41 @@
 (function($){  
-	$.fn.atabs = function(options) { 
-		var me = $(this);
-		function handle_menu_click(click_event){
-			//run the css test
-			var csstest = $('.hidden').css('position');
-			if (csstest !== 'absolute') {return;}
-			// We don't want the link to act normally
-			click_event.preventDefault();
-			// assign variable for href
-		    var href = $(this).attr('href').replace("#", "");
-			// if already active or href equals #blank dont change tab, just return
-		   	if ($(this).parents('li').hasClass('active') || href === "#blank" || $(this).parents('li').hasClass('disabled')) {return;}
-			me.find('.active').removeClass('active');
-			$(this).parent('li').addClass('active');
-			me.find('.open').addClass('hidden').removeClass('open');
-			$('a[name='+href+']').parent('.target').removeClass('hidden').addClass('open');
-		}
-		
-		
-		me.find('.trigger').find('a').click(handle_menu_click);
-		// hide all targets except the default open
-		me.find('.target').each(function(){
-			$(this).find('.topLink').addClass('hidden');
-			if (!$(this).hasClass('open')) {
-				$(this).addClass('hidden');
+	$.fn.atabs = function(options) {
+		var o = { defaultTab:1, mode:"tabs" }
+		$.extend(o,options);
+		var methods = {
+			click: function ($me,$parent,e) {
+				e.preventDefault();
+				var href = $me.attr('href');
+				if (($me.parents('.trigger').hasClass('active') || $me.parents('.trigger').hasClass('disabled')) && o.mode === "tabs") {return;}
+				if ($me.parents('.trigger').hasClass('disabled') && o.mode === "accordion") {return;}
+				$me.parents('.trigger').toggleClass("active");
+				
+				if (o.mode === "tabs") {
+					$parent.find('.active').removeClass('active'); 
+					$me.parents('.trigger').addClass("active"); 
+					$parent.find('.open').addClass('hidden').removeClass('open'); 
+					$(href).removeClass('hidden').addClass('open');
+				}
+				if (o.mode === "accordion") { $(href).toggleClass('hidden'); }
 			}
-		});
+		};
+
+		return this.each(
+			function () {
+				var $this = $(this);
+				o.defaultTab = o.defaultTab-1;
+				$this.find('.trigger').find('a').unbind('click').bind('click', function (e) { methods.click($(this),$this,e) } );
+				$this.find('.target').each(
+					function (i) {
+						$(this).find('.topLink').addClass('hidden');
+						if (i !== o.defaultTab) { $(this).addClass('hidden'); }
+						else if (i === o.defaultTab) {
+							$(this).addClass('open');
+							$this.find('.trigger').eq(o.defaultTab).addClass('active');
+						}
+					}
+				);
+			}
+		);
 	};
 })(jQuery);
